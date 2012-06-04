a0778327a23b9d7a8c7058a7075a1a06864c05db
Alyx <alyx@malkier.net>
2012-06-04 08:25:10 -0700

    Fix this to be POSIX.1-2008 compliant

--- include/assert.h
+++ include/assert.h
@@ -1,11 +1,17 @@
 #pragma once
 
+#ifdef NDEBUG
+
 #include "stdlib.h"
 #include "stdio.h"
 
 #define assert(x) \
-    if (!x) { \
+    if (!x) \
+    { \
         fprintf(stderr, "\"%s\" failed at %s:%d\n", #x, __FILE__, __LINE__); \
-        abort();
+        abort(); \
+    }
 
-
+#else
+#define assert(ignore) ((void) 0)
+#endif
